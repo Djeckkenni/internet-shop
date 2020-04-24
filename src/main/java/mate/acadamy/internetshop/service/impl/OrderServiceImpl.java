73a19f46618728174bd292214bb8297c3b05a655
Djeckkenni <djeck27kenni@gmail.com>
2020-04-24 17:09:01 +0300

    correct code-2

--- src/main/java/mate/acadamy/internetshop/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/acadamy/internetshop/service/impl/OrderServiceImpl.java
@@ -2,25 +2,33 @@ package mate.acadamy.internetshop.service.impl;
 
 import java.util.List;
 import mate.acadamy.internetshop.dao.OrderDao;
+import mate.acadamy.internetshop.dao.ProductDao;
+import mate.acadamy.internetshop.dao.UserDao;
 import mate.acadamy.internetshop.inject.lib.Inject;
 import mate.acadamy.internetshop.model.Order;
 import mate.acadamy.internetshop.model.Product;
 import mate.acadamy.internetshop.model.User;
 import mate.acadamy.internetshop.service.OrderService;
+import mate.acadamy.internetshop.service.ShoppingCartService;
 import mate.acadamy.internetshop.service.lib.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
+    @Inject
+    private UserDao userDao;
 
+    @Inject
+    private ProductDao productDao;
     @Override
     public Order completeOrder(List<Product> products, User user) {
         Order order = new Order();
         order.setProducts(products);
         order.setUser(user);
-        orderDao.create(order);
-        orderDao.getUserOrders(user.getUserId()).clear();
+        //shoppingCartService.clear(shoppingCartService.getByUserId(user.getUserId()));
         return order;
     }
 
--- src/main/java/mate/acadamy/internetshop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/acadamy/internetshop/service/impl/ShoppingCartServiceImpl.java
@@ -1,30 +1,32 @@
 package mate.acadamy.internetshop.service.impl;
 
 import java.util.List;
+
 import mate.acadamy.internetshop.dao.ShoppingCartDao;
 import mate.acadamy.internetshop.db.Storage;
 import mate.acadamy.internetshop.inject.lib.Inject;
 import mate.acadamy.internetshop.model.Product;
 import mate.acadamy.internetshop.model.ShoppingCart;
 import mate.acadamy.internetshop.service.ShoppingCartService;
+import mate.acadamy.internetshop.service.UserService;
 import mate.acadamy.internetshop.service.lib.Service;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Inject
     private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private UserService userService;
 
     @Override
     public ShoppingCart addProduct(ShoppingCart shoppingCart, Product product) {
-        shoppingCartDao.create(shoppingCart);
         shoppingCart.getProducts().add(product);
         return shoppingCartDao.update(shoppingCart);
     }
 
     @Override
     public boolean deleteProduct(ShoppingCart shoppingCart, Product product) {
-        if (shoppingCart.getProducts().removeIf(product1 -> product.getProductId()
-                .equals(product1.getProductId()))) {
+        if (shoppingCart.getProducts().remove(product)) {
             shoppingCartDao.update(shoppingCart);
             return true;
         }
@@ -39,14 +41,14 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public ShoppingCart getByUserId(Long userId) {
-        return shoppingCartDao.getByUserId(userId).get();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(getByUserId(userId).getUser());
+        shoppingCart.setProducts(getByUserId(userId).getProducts());
+        return shoppingCartDao.getByUserId(userId).orElseGet(() -> shoppingCartDao.create(shoppingCart));
     }
 
     @Override
     public List<Product> getAllProducts(ShoppingCart shoppingCart) {
-        return Storage.shoppingCarts
-                .stream()
-                .filter(cart -> cart.getShoppingCartId().equals(shoppingCart.getShoppingCartId()))
-                .findFirst().get().getProducts();
+        return shoppingCart.getProducts();
     }
 }
