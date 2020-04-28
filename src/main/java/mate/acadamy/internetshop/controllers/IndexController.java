f4b49954920ae18a5ec9146ed9a16101214e3f20
Djeckkenni <djeck27kenni@gmail.com>
2020-04-28 16:06:36 +0300

    test commit-1

--- src/main/java/mate/acadamy/internetshop/controllers/IndexController.java
+++ src/main/java/mate/acadamy/internetshop/controllers/IndexController.java
@@ -1,11 +1,11 @@
 package mate.acadamy.internetshop.controllers;
 
+import java.io.IOException;
+import java.time.LocalTime;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.time.LocalTime;
 
 public class IndexController extends HttpServlet {
     @Override
@@ -13,6 +13,18 @@ public class IndexController extends HttpServlet {
             throws ServletException, IOException {
         String timeValue = LocalTime.now().toString();
         req.setAttribute("time", timeValue);
-        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
+
+
+/*<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Demo</title>
+</head>
+<body>
+<h1>Your bock  data was added to DB</h1>
+<a href="${pageContext.request.contextPath}/">Go to the main page</a>
+</body>
+</html>*/
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,9 +1,14 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Mate Academy!</title>
 </head>
 <body>
-    <h1>Hello world! Current time is ${time}</h1>
+    <h1>Hello mates! Current time is ${time}</h1>
+    <button type="button"
+            onclick='location.href="/injectData"'>Generate Users
+    </button>
+
 </body>
 </html>
+
